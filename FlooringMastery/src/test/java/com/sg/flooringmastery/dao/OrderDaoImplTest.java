ae89fd7982532aa032d0de967db9b45b4a077e63
OtelDeraj <Isaiah_Evans_M@live.com>
2020-06-12 13:43:34 -0500

    golden path tests for add, edit, and get order methods now assert across all fields. Ready for review.

--- FlooringMastery/src/test/java/com/sg/flooringmastery/dao/OrderDaoImplTest.java
+++ FlooringMastery/src/test/java/com/sg/flooringmastery/dao/OrderDaoImplTest.java
@@ -111,7 +111,18 @@ public class OrderDaoImplTest {
         try {
             FMOrder testOrder = od.getOrder(LocalDate.of(2020, 9, 8), 1);
 
+            assertEquals(1, testOrder.getOrderNum());
             assertEquals("Ada Lovelace", testOrder.getCustomerName());
+            assertEquals("CA", testOrder.getTaxRate().getStateAbv());
+            assertEquals(new BigDecimal("25.00"), testOrder.getTaxRate().getStateTaxRate());
+            assertEquals("Tile", testOrder.getProduct().getMaterial());
+            assertEquals(new BigDecimal("249.00"), testOrder.getArea());
+            assertEquals(new BigDecimal("3.50"), testOrder.getProduct().getCostPerSqFt());
+            assertEquals(new BigDecimal("4.15"), testOrder.getProduct().getLaborCostPerSqFt());
+            assertEquals(new BigDecimal("871.50"), testOrder.getMaterialCost());
+            assertEquals(new BigDecimal("1033.35"), testOrder.getLaborCost());
+            assertEquals(new BigDecimal("476.21"), testOrder.getSalesTax());
+            assertEquals(new BigDecimal("2381.06"), testOrder.getTotalCost());
 
         } catch (OrderDaoException ex) {
             fail("Hit OrderDaoExcepion in golden path.");
@@ -183,7 +194,17 @@ public class OrderDaoImplTest {
 
             od.addOrder(toAdd);
             FMOrder toCheck = od.getOrder(date, 3);
-            assertEquals("Martha Stewart", toCheck.getCustomerName());
+            assertEquals(3, toCheck.getOrderNum());
+            assertEquals(toAdd.getCustomerName(), toCheck.getCustomerName());
+            assertEquals(toAdd.getTaxRate().getStateAbv(), toCheck.getTaxRate().getStateAbv());
+            assertEquals(toAdd.getTaxRate().getStateTaxRate(), toCheck.getTaxRate().getStateTaxRate());
+            assertEquals(toAdd.getProduct().getMaterial(), toCheck.getProduct().getMaterial());
+            assertEquals(toAdd.getProduct().getCostPerSqFt(), toCheck.getProduct().getCostPerSqFt());
+            assertEquals(toAdd.getProduct().getLaborCostPerSqFt(), toCheck.getProduct().getLaborCostPerSqFt());
+            assertEquals(toAdd.getMaterialCost(), toCheck.getMaterialCost());
+            assertEquals(toAdd.getLaborCost(), toCheck.getLaborCost());
+            assertEquals(toAdd.getSalesTax(), toCheck.getSalesTax());
+            assertEquals(toAdd.getTotalCost(), toCheck.getTotalCost());
         } catch (OrderDaoException ex) {
             fail("should not throw OrderDaoException in golden path");
         } catch (InvalidOrderDateException ex) {
@@ -220,12 +241,25 @@ public class OrderDaoImplTest {
     @Test
     public void testEditOrderGoldenPath() {
         try {
-            FMOrder toEdit = od.getOrder(date, 2);
-            BigDecimal chonk = toEdit.getArea().add(new BigDecimal("10"));
-            toEdit.setArea(chonk);
+            FMOrder rawOrder = od.getOrder(date, 2);
+            BigDecimal alteredArea = rawOrder.getArea().add(new BigDecimal("10"));
+            rawOrder.setArea(alteredArea);
+            FMOrder toEdit = new FMOrder(rawOrder, rawOrder.getProduct(), rawOrder.getTaxRate());
             od.editOrder(toEdit);
             FMOrder toCheck = od.getOrder(date, 2);
-            assertEquals(chonk, toCheck.getArea());
+            assertEquals(toEdit.getOrderNum(), toCheck.getOrderNum());
+            assertEquals(toEdit.getCustomerName(), toCheck.getCustomerName());
+            assertEquals(toEdit.getTaxRate().getStateAbv(), toCheck.getTaxRate().getStateAbv());
+            assertEquals(toEdit.getTaxRate().getStateTaxRate(), toCheck.getTaxRate().getStateTaxRate());
+            assertEquals(toEdit.getProduct().getMaterial(), toCheck.getProduct().getMaterial());
+            assertEquals(toEdit.getArea(), toCheck.getArea());
+            assertEquals(toEdit.getProduct().getCostPerSqFt(), toCheck.getProduct().getCostPerSqFt() );
+            assertEquals(toEdit.getProduct().getLaborCostPerSqFt(), toCheck.getProduct().getLaborCostPerSqFt());
+            assertEquals(toEdit.getMaterialCost(), toCheck.getMaterialCost());
+            assertEquals(toEdit.getLaborCost(), toCheck.getLaborCost());
+            assertEquals(toEdit.getSalesTax(), toCheck.getSalesTax());
+            assertEquals(toEdit.getTotalCost(), toCheck.getTotalCost());
+
         } catch (OrderDaoException ex) {
             fail("Should not hit OrderDaoException in golden path");
         } catch (InvalidOrderDateException ex) {
@@ -264,7 +298,7 @@ public class OrderDaoImplTest {
             od.editOrder(toEdit);
             fail("Should have it OrderDaoException");
         } catch (OrderDaoException ex) {
-            
+
         } catch (InvalidOrderDateException ex) {
             fail("Should not hit InvalidOrderDateException");
         } catch (InvalidInputException ex) {
@@ -286,7 +320,7 @@ public class OrderDaoImplTest {
         } catch (InvalidOrderDateException ex) {
             fail("Should not hit InvalidOrderDateException");
         } catch (InvalidInputException ex) {
-            
+
         }
     }
 
@@ -313,9 +347,9 @@ public class OrderDaoImplTest {
         }
 
     }
-    
+
     @Test
-    public void testRemoveOrderNoFile(){
+    public void testRemoveOrderNoFile() {
         try {
             FMOrder toRemove = od.getOrder(date, 2);
             toRemove.setDate(LocalDate.of(2020, 2, 3));
@@ -329,9 +363,9 @@ public class OrderDaoImplTest {
             fail("Should not hit InvalidInputException in golden path");
         }
     }
-    
-     @Test
-    public void testRemoveOrderNullDate(){
+
+    @Test
+    public void testRemoveOrderNullDate() {
         try {
             FMOrder toRemove = od.getOrder(date, 2);
             toRemove.setDate(null);
@@ -345,9 +379,9 @@ public class OrderDaoImplTest {
             fail("Should not hit InvalidInputException in golden path");
         }
     }
-    
-     @Test
-    public void testRemoveOrderBadOrderNum(){
+
+    @Test
+    public void testRemoveOrderBadOrderNum() {
         try {
             FMOrder toRemove = od.getOrder(date, 2);
             toRemove.setOrderNum(99);
@@ -358,7 +392,7 @@ public class OrderDaoImplTest {
         } catch (InvalidOrderDateException ex) {
             fail("Should not hit InvalidOrderDateException in golden path");
         } catch (InvalidInputException ex) {
-            
+
         }
     }
 
